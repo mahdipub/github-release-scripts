6546ef009bbb55435812dbb9b9bd8ee16c1d38ea
mahdi@ibm.com <136717340+mahdipub@users.noreply.github.com>
2025-04-07 21:45:31 -0400

    Update UploadFiles.groovy

--- adopt-github-release/src/main/groovy/net/adoptium/release/UploadFiles.groovy
+++ adopt-github-release/src/main/groovy/net/adoptium/release/UploadFiles.groovy
@@ -19,30 +19,39 @@ class UploadAdoptReleaseFiles {
     private final String version
     private final String server
     private final String org
-    private final String edition
+    private final boolean isDevKit
+    private final boolean isPkgSrc
 
-    UploadAdoptReleaseFiles(String tag, String description, boolean release, String version, String server, String org, String edition, List<File> files) {
+    UploadAdoptReleaseFiles(String tag, String description, boolean release, boolean isDevKit, boolean isPkgSrc, String version, String server, String org, List<File> files) {
         this.tag = tag
         this.description = description
         this.release = release
+        this.isDevKit = isDevKit
+        this.isPkgSrc = isPkgSrc
         this.files = files
         this.version = version
         this.server = server
         this.org = org
-        this.edition = edition
     }
 
     void release() {
-        def grouped = files.groupBy {
-            switch (it.getName()) {
-                case ~/.*semeru.*/: "ibm"; break;
-            }
-        }
-        GHRepository repo = getRepo(edition)
-        println("REPO:$repo")
+        GHRepository repo = getRepo("adopt")
         GHRelease release = getRelease(repo)
-        println("RELEASE:$release")
-        uploadFiles(release, grouped.get("ibm"))
+        if (isDevKit || isPkgSrc) {
+          uploadFiles(release, files)
+        } else {
+          def grouped = files.groupBy {
+            switch (it.getName()) {
+                // Only release file names containing certain patterns
+                // to avoid publication of non-temurin builds
+                case ~/.*hotspot.*/: "adopt"; break;
+                case ~/.*sources.*/: "adopt"; break;
+                case ~/.*release-notes.*/: "adopt"; break;
+                case ~/.*AQAvitTapFiles.*/: "adopt"; break;
+            }
+          }
+          uploadFiles(release, grouped.get("adopt"))
+        }
     }
 
     private GHRepository getRepo(String vendor) {
@@ -65,18 +74,22 @@ class UploadAdoptReleaseFiles {
                         (int) TimeUnit.SECONDS.toMillis(120)))
 
         println("Using Github org:'${org}'")
-        // jdk11 => 11
-        def numberVersion = version.replaceAll(/[^0-9]/, "")
-        def repoName = "${org}/temurin${numberVersion}-binaries"
 
-        if (vendor == "open") {
-            repoName = "${org}/semeru${numberVersion}-binaries"
-        } else if (vendor == "certified") {
-            repoName = "${org}/semeru${numberVersion}-certified-binaries"
-        } else if (vendor == "ea") {
-            repoName = "${org}/semeru${numberVersion}-ea-binaries"
+        def repoName
+        if (isDevKit) {
+          repoName = "${org}/devkit-binaries"
+        } else if (isPkgSrc) {
+          repoName = "${org}/temurin-linux-pkg-sources"
+        } else {
+          // jdk11 => 11
+          def numberVersion = version.replaceAll(/[^0-9]/, "")
+          repoName = "${org}/temurin${numberVersion}-binaries"
+
+          if (vendor != "adopt") {
+              repoName = "${org}/open${version}-${vendor}-binaries"
+          }
         }
-        println("reponame:${repoName}")
+
         return github.getRepository(repoName)
     }
 
@@ -92,36 +105,22 @@ class UploadAdoptReleaseFiles {
                     }
 
             println("Uploading ${file.name}")
-
-            // Determine the MIME type explicitly for .msi files
-            String mimeType = 'application/octet-stream'  // Default MIME type
-            if (file.name.endsWith('.msi')) {
-                mimeType = 'application/x-msi'  // Explicit MIME type for .msi files
-            } else {
-                mimeType = Files.probeContentType(file.toPath())  // Auto-detect MIME type for other files
-            }
-
-            // Upload the file with the determined MIME type
-            release.uploadAsset(file, mimeType)
+            release.uploadAsset(file, Files.probeContentType(file.toPath()))
         }
     }
 
     private GHRelease getRelease(GHRepository repo) {
-        println("get releases")
         GHRelease release = repo
                 .getReleaseByTagName(tag)
-        println("got releases")
+
         if (release == null) {
-            println("releases was null")
             release = repo
                     .createRelease(tag)
                     .body(description)
-                    .draft(true)
                     .name(tag)
                     .prerelease(!this.release)
                     .create()
         }
-        println("Done getting release")
         return release
     }
 }
@@ -137,10 +136,11 @@ static void main(String[] args) {
             options.t,
             options.d,
             options.r,
+            options.k,
+            options.p,
             options.v,
             options.s,
             options.o,
-            options.p,
             files,
     ).release()
 }
@@ -155,10 +155,11 @@ private OptionAccessor parseArgs(String[] args) {
                 t longOpt: 'tag', type: String, args: 1, 'Tag name'
                 d longOpt: 'description', type: String, args: 1, 'Release description'
                 r longOpt: 'release', 'Is a release build'
+                k longOpt: 'isDevKit', 'Is a DevKit build'
+                p longOpt: 'isPkgSrc', 'Is a linux package src build'
                 h longOpt: 'help', 'Show usage information'
                 s longOpt: 'server', type: String, args: 1, optionalArg: true, defaultValue: 'https://api.github.com', 'Github server'
                 o longOpt: 'org', type: String, args: 1, optionalArg: true, defaultValue: 'adoptium', 'Github org'
-                p longOpt: 'edition', type: String, args: 1, optionalArg: true, defaultValue: 'open', 'Semeru Edition'
             }
 
     def options = cliBuilder.parse(args)
